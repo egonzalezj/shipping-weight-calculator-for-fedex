86f5c46e28c715ed3d6b8c5994cdccb4ab722cce
Elisabet Gonzalez <sistemas@procupros.com>
2017-09-06 13:35:08 -0600

    Configuración inicial

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  Plugin Name: Woocommerce Shipping MX
+  Plugin URI: http://github.com/egonzalezj
+  Description: Herramienta para configurar métodos de envío en México.
+  Author: Elisabet González
+  Version: 1.0
+  Author URI: http://github.com/egonzalezj
+*/
+?>
